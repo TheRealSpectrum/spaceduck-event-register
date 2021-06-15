716638a1f829087a56aeee4d2c6e498da9d23bbb
Patricia <poudenaarden@outlook.com>
2021-06-15 14:01:32 +0200

    update migrations

---
+++ database/migrations/2021_06_15_114147_create_events_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('organiser');
+            $table->string ('title');
+            $table->dateTime('date_event');
+            $table->string('location');
+            $table->text('description');
+            $table->integer('limit_visitors');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
